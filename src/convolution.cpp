bff27f8d559827bd9160a5086bd075f239b5c7ec
sameersul <sameerjuin@gmail.com>
2025-05-03 15:44:39 -0400

    updated convolution to hopefully work with gaussian kernel

--- src/convolution.cpp
+++ src/convolution.cpp
@@ -6,28 +6,48 @@ void Convolution(vector<pair<array<int, 3>, float>> &output, vector<pair<array<i
 {
     cout << "entering convolution" << endl;
     output.clear();
-    output.resize(input.size() + psf.size() - 1, make_pair(array<int, 3>{0, 0, 0}, 0.0f)); // Explicitly construct the pair to avoid dumb error
+    output.resize(input.size(), make_pair(array<int, 3>{0, 0, 0}, 0.0f)); // Initialize output with default values
     cout << "output size: " << output.size() << endl;
-    int counter = 0;
-    for (size_t i = 0; i < input.size(); i++) {
-        for (size_t j = 0; j < psf.size(); j++) {
-            if (i + j < output.size()) { // Ensure index is within bounds
-                output[i + j].first[0] += input[i].first[0] * psf[j].first; // Red
-                output[i + j].first[1] += input[i].first[1] * psf[j].first; // Green
-                output[i + j].first[2] += input[i].first[2] * psf[j].first; // Blue
 
-                output[i + j].second += input[i].second * psf[j].second; // Grayscale intensity
-                counter++;
+    int width = static_cast<int>(sqrt(input.size())); // Assuming input is a square image
+    int height = width; // For simplicity, width = height
+    int psfSize = static_cast<int>(sqrt(psf.size())); // PSF is a square matrix
+    int psfHalf = psfSize / 2;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            array<float, 3> rgbSum = {0.0f, 0.0f, 0.0f};
+            float intensitySum = 0.0f;
+
+            // Apply the PSF (Gaussian kernel)
+            for (int ky = -psfHalf; ky <= psfHalf; ky++) {
+                for (int kx = -psfHalf; kx <= psfHalf; kx++) {
+                    int pixelX = x + kx;
+                    int pixelY = y + ky;
+
+                    // Check bounds
+                    if (pixelX >= 0 && pixelX < width && pixelY >= 0 && pixelY < height) {
+                        int pixelIndex = pixelY * width + pixelX;
+                        int psfIndex = (ky + psfHalf) * psfSize + (kx + psfHalf);
+
+                        rgbSum[0] += input[pixelIndex].first[0] * psf[psfIndex].first; // Red
+                        rgbSum[1] += input[pixelIndex].first[1] * psf[psfIndex].first; // Green
+                        rgbSum[2] += input[pixelIndex].first[2] * psf[psfIndex].first; // Blue
+                        intensitySum += input[pixelIndex].second * psf[psfIndex].second; // Grayscale intensity
+                    }
+                }
             }
+
+            // Store the result in the output
+            int outputIndex = y * width + x;
+            output[outputIndex].first[0] = static_cast<int>(rgbSum[0]);
+            output[outputIndex].first[1] = static_cast<int>(rgbSum[1]);
+            output[outputIndex].first[2] = static_cast<int>(rgbSum[2]);
+            output[outputIndex].second = intensitySum;
         }
     }
 
-    // Resize to match the input size after convolution
-    if (output.size() > input.size()) {
-        output.resize(input.size());
-    }
-
-    cout << "resized to " << input.size() << " pixels and finished convolution" << endl;
+    cout << "finished convolution" << endl;
 }
 
 // can use classic randomly determined impulse response for psf later....
--- src/convolution.h
+++ src/convolution.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <bitset>
 #include <array>
+#include <cmath>
 
 using namespace std;
 
